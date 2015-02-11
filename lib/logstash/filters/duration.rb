320cc05ba639cf8e148b3043098dc06477ede3d4
Benoit Brayer <benoit.brayer@st.com>
2015-02-11 12:38:42 +0100

    First Commit : Init logstash duration filter folder hierarchy

---
+++ lib/logstash/filters/duration.rb
@@ -0,0 +1,120 @@
+require "logstash/filters/base"
+require "logstash/namespace"
+require "date"
+
+# The filter will calculate the duration between two given dates (first_date and last_date).
+#
+# The goal of this filter is to add a new field containing the value of the time interval (in seconds) between the two given dates.
+#
+# The date formats allowed are anything allowed by Joda-Time (java time library). You can see the docs for this format here:
+#
+# [joda.time.format.DateTimeFormat](http://joda-time.sourceforge.net/apidocs/org/joda/time/format/DateTimeFormat.html)
+#
+# The config looks like this:
+#     filter {
+#       duration {
+#         field_name => "new field name"
+#         first_date => ["first_logdate", "MMM dd YYY HH:mm:ss"]
+#         second_date => ["second_logdate", "MMM dd YYY HH:mm:ss"]
+#       }
+#     }
+#
+class LogStash::Filters::Duration < LogStash::Filters::Base
+  if RUBY_ENGINE == "jruby"
+    JavaException = java.lang.Exception
+    UTC = org.joda.time.DateTimeZone.forID("UTC")
+  end
+
+  # filter {
+  #   duration {
+  #     field_name => ... # string (optionnal), default: "duration"
+  #     first_date => ... # hash (requiered), default: {}
+  #     second_date => ... # hash (requiered), default: {}
+  #   }
+  # }
+  config_name "duration"
+  milestone 1
+
+  # The name of the new field.
+  config :field_name, :validate => :string
+
+  # The pair of first log date filed name and the date pattern following Joda-Time (java time library):
+  # [joda.time.format.DateTimeFormat](http://joda-time.sourceforge.net/apidocs/org/joda/time/format/DateTimeFormat.html)
+  config :first_date, :validate => :array, :default => []
+
+  # The pair of second log date filed name and the date pattern following Joda-Time (java time library):
+  # [joda.time.format.DateTimeFormat](http://joda-time.sourceforge.net/apidocs/org/joda/time/format/DateTimeFormat.html)
+  config :second_date, :validate => :array, :default => []
+
+  public
+  def register
+    require "java"
+    if @first_date.length < 2
+      raise LogStash::ConfigurationError, I18n.t("logstash.agent.configuration.invalid_plugin_register",
+        :plugin => "filter", :type => "duration",
+        :error => "The first_date setting should contains first a field name and one date format, current value is #{@first_date}")
+    end
+    if @second_date.length < 2
+      raise LogStash::ConfigurationError, I18n.t("logstash.agent.configuration.invalid_plugin_register",
+        :plugin => "filter", :type => "duration",
+        :error => "The second_date setting should contains first a field name and one date format, current value is #{@second_date}")
+    end
+  end # def register
+
+  private
+  def setupMatcher(field, value)
+    value.each do |format|
+      case format
+        when "ISO8601"
+          joda_parser = org.joda.time.format.ISODateTimeFormat.dateTimeParser
+          if @timezone
+            joda_parser = joda_parser.withZone(org.joda.time.DateTimeZone.forID(@timezone))
+          else
+            joda_parser = joda_parser.withOffsetParsed
+          end
+          parser = lambda { |date| joda_parser.parseMillis(date) }
+        when "UNIX" # unix epoch
+          joda_instant = org.joda.time.Instant.java_class.constructor(Java::long).method(:new_instance)
+          #parser = lambda { |date| joda_instant.call((date.to_f * 1000).to_i).to_java.toDateTime }
+          parser = lambda { |date| (date.to_f * 1000).to_i }
+        when "UNIX_MS" # unix epoch in ms
+          joda_instant = org.joda.time.Instant.java_class.constructor(Java::long).method(:new_instance)
+          parser = lambda do |date|
+            #return joda_instant.call(date.to_i).to_java.toDateTime
+            return date.to_i
+          end
+        when "TAI64N" # TAI64 with nanoseconds, -10000 accounts for leap seconds
+          joda_instant = org.joda.time.Instant.java_class.constructor(Java::long).method(:new_instance)
+          parser = lambda do |date|
+            # Skip leading "@" if it is present (common in tai64n times)
+            date = date[1..-1] if date[0, 1] == "@"
+            #return joda_instant.call((date[1..15].hex * 1000 - 10000)+(date[16..23].hex/1000000)).to_java.toDateTime
+            return (date[1..15].hex * 1000 - 10000)+(date[16..23].hex/1000000)
+          end
+        else
+          joda_parser = org.joda.time.format.DateTimeFormat.forPattern(format).withDefaultYear(Time.new.year)
+          if @timezone
+            joda_parser = joda_parser.withZone(org.joda.time.DateTimeZone.forID(@timezone))
+          else
+            joda_parser = joda_parser.withOffsetParsed
+          end
+          parser = lambda { |date| joda_parser.parseMillis(date) }
+      end
+      @logger.debug("Adding type with date config", :type => @type,
+                    :field => field, :format => format)
+      @parsers[field] << {
+        :parser => parser,
+        :format => format
+      }
+  end
+
+  public
+  def filter(event)
+    # return nothing unless there's an actual filter even
+    return unless filter?(event)
+    if @field_name
+      event[@field_name] = "duration"
+    end
+    filter_matched(event)
+  end # def filter
+end # class LogStash::Filters::Duration
