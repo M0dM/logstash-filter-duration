c59c24cec68289d31ee3c2bfab707d60852acaf8
Benoit Brayer <benoit.brayer@st.com>
2015-02-11 14:43:09 +0100

    Adding : - the two Hash variable for both first_date and second_date parsers - setupMatcher initialization code to init those two parses hash variable
    Signed-off-by: Benoit Brayer <benoit.brayer@st.com>

--- lib/logstash/filters/duration.rb
+++ lib/logstash/filters/duration.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require "logstash/filters/base"
 require "logstash/namespace"
 require "date"
@@ -47,6 +48,14 @@ class LogStash::Filters::Duration < LogStash::Filters::Base
   config :second_date, :validate => :array, :default => []
 
   public
+  def initialize(config = {})
+    super
+
+    @first_parsers = Hash.new { |h,k| h[k] = [] }
+    @second_parsers = Hash.new { |h,k| h[k] = [] }
+  end # def initialize
+
+  public
   def register
     require "java"
     if @first_date.length < 2
@@ -59,12 +68,13 @@ class LogStash::Filters::Duration < LogStash::Filters::Base
         :plugin => "filter", :type => "duration",
         :error => "The second_date setting should contains first a field name and one date format, current value is #{@second_date}")
     end
+    setupMatcher(@config["first_date"].shift, @config["first_date"], @config["second_date"].shift,  @config["second_date"])
   end # def register
 
   private
-  def setupMatcher(field, value)
-    value.each do |format|
-      case format
+  def setupMatcher(first_field, first_value, second_field, second_value)
+    first_value.each do |first_format|
+      case first_format
         when "ISO8601"
           joda_parser = org.joda.time.format.ISODateTimeFormat.dateTimeParser
           if @timezone
@@ -92,7 +102,7 @@ class LogStash::Filters::Duration < LogStash::Filters::Base
             return (date[1..15].hex * 1000 - 10000)+(date[16..23].hex/1000000)
           end
         else
-          joda_parser = org.joda.time.format.DateTimeFormat.forPattern(format).withDefaultYear(Time.new.year)
+          joda_parser = org.joda.time.format.DateTimeFormat.forPattern(first_format).withDefaultYear(Time.new.year)
           if @timezone
             joda_parser = joda_parser.withZone(org.joda.time.DateTimeZone.forID(@timezone))
           else
@@ -101,10 +111,51 @@ class LogStash::Filters::Duration < LogStash::Filters::Base
           parser = lambda { |date| joda_parser.parseMillis(date) }
       end
       @logger.debug("Adding type with date config", :type => @type,
-                    :field => field, :format => format)
-      @parsers[field] << {
+                    :field => first_field, :format => first_format)
+      @first_parsers[first_field] << {
         :parser => parser,
-        :format => format
+        :format => first_format
+      }
+    second_value.each do |second_format|
+      case second_format
+        when "ISO8601"
+          joda_parser = org.joda.time.format.ISODateTimeFormat.dateTimeParser
+          if @timezone
+            joda_parser = joda_parser.withZone(org.joda.time.DateTimeZone.forID(@timezone))
+          else
+            joda_parser = joda_parser.withOffsetParsed
+          end
+          parser = lambda { |date| joda_parser.parseMillis(date) }
+        when "UNIX" # unix epoch
+          joda_instant = org.joda.time.Instant.java_class.constructor(Java::long).method(:new_instance)
+          parser = lambda { |date| (date.to_f * 1000).to_i }
+        when "UNIX_MS" # unix epoch in ms
+          joda_instant = org.joda.time.Instant.java_class.constructor(Java::long).method(:new_instance)
+          parser = lambda do |date|
+            return date.to_i
+          end
+        when "TAI64N" # TAI64 with nanoseconds, -10000 accounts for leap seconds
+          joda_instant = org.joda.time.Instant.java_class.constructor(Java::long).method(:new_instance)
+          parser = lambda do |date|
+          # Skip leading "@" if it is present (common in tai64n times)
+          date = date[1..-1] if date[0, 1] == "@"
+          #return joda_instant.call((date[1..15].hex * 1000 - 10000)+(date[16..23].hex/1000000)).to_java.toDateTime
+          return (date[1..15].hex * 1000 - 10000)+(date[16..23].hex/1000000)
+        end
+      else
+        joda_parser = org.joda.time.format.DateTimeFormat.forPattern(second_format).withDefaultYear(Time.new.year)
+          if @timezone
+            joda_parser = joda_parser.withZone(org.joda.time.DateTimeZone.forID(@timezone))
+          else
+            joda_parser = joda_parser.withOffsetParsed
+          end
+          parser = lambda { |date| joda_parser.parseMillis(date) }
+      end
+      @logger.debug("Adding type with date config", :type => @type,
+                    :field => second_field, :format => second_format)
+      @second_parsers[second_field] << {
+        :parser => parser,
+        :format => second_format
       }
   end
 
